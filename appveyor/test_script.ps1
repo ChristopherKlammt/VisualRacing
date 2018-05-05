fe180aaf5546e6038e30643433f4462090a3e1c8
Christopher <christopher.klammt@gmail.com>
2018-05-05 20:37:25 +0200

    Test

--- appveyor/test_script.ps1
+++ appveyor/test_script.ps1
@@ -3,13 +3,17 @@ echo "Running Tests"
 .\tst_vrtest.exe
 .\tst_vrtest.exe -xunitxml > tests.xml
 
-$folder = Get-ChildItem -Path *.cpp
+#$folder = Get-ChildItem -Path *.cpp
 
 # Loop through each file
-foreach ($file in $folder) {
-    gcov $file
-}
-dir
+#foreach ($file in $folder) {
+#    gcov $file
+#}
+#dir
+echo "Running gcovr"
 gcovr -r
+
+echo "Running gcovr XML"
+gcovr -r -x
 
 popd
