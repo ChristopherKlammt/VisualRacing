3caf455f3bcd0f5d94b27701c1b647905890f672
Christopher <christopher.klammt@gmail.com>
2018-05-05 22:50:50 +0200

    Upload Coverage to Codecov

--- appveyor/test_script.ps1
+++ appveyor/test_script.ps1
@@ -16,6 +16,9 @@ python C:\Python27\Scripts\gcovr
 echo "Running gcovr XML"
 python C:\Python27\Scripts\gcovr -x > coverage.xml
 
+Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile codecov.sh
+bash codecov.sh -f "coverage.xml"
+
 dir
 
 popd
