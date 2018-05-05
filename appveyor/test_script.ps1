9052371b4cd60b87ad3b435294b13f38d9f6a71b
Christopher <christopher.klammt@gmail.com>
2018-05-05 22:57:25 +0200

    Working on Format

--- appveyor/test_script.ps1
+++ appveyor/test_script.ps1
@@ -14,7 +14,7 @@ echo "Running gcovr"
 python C:\Python27\Scripts\gcovr
 
 echo "Running gcovr XML"
-python C:\Python27\Scripts\gcovr -x > coverage.xml
+python C:\Python27\Scripts\gcovr --xml -o coverage.xml
 
 Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile codecov.sh
 bash codecov.sh -f "coverage.xml"
