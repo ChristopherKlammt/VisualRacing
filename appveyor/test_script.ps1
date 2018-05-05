ec9c4393a62bf9375dd0bed6e4e69b9666111c21
Christopher <christopher.klammt@gmail.com>
2018-05-05 19:19:38 +0200

    GCov

--- appveyor/test_script.ps1
+++ appveyor/test_script.ps1
@@ -2,5 +2,13 @@ pushd VRTest/debug
 echo "Running Tests"
 .\tst_vrtest.exe
 .\tst_vrtest.exe -xunitxml > tests.xml
+
+$folder = Get-ChildItem -Path *.cpp
+
+# Loop through each file
+foreach ($file in $folder) {
+    gcov $file
+}
 dir
+
 popd
